d21df626cb65a6809d4e0c5859fd5cec36c24157
DedekindCuts <37382607+DedekindCuts@users.noreply.github.com>
2018-03-14 14:40:43 -0600

    Add days_ago_count to config.py

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ host = "XXX"
 
 #Pendo API Key
 pendo_key = "XXX"
+
+#Number of days' worth of data to access from Pendo; counts the number of days before today.
+#So to get only data from yesterday, set days_ago_count = 1, to get data from yesterday and the day before, set days_ago_count = 2, etc.
+days_ago_count = 0
